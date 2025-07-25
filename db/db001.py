2a242a278a5c4182bce7cc13cb6e7914da90c046
gtbnhyujmj <gtbnhyujmj@gmail.com>
2025-07-25 13:14:24 +0800

    GPT設計好的使用者註冊模板，給未來複製用。
    GPT設計好的使用者註冊模板，給未來複製用。

---
+++ db/db001.py
@@ -0,0 +1,15 @@
+# 存放GPT設計好的資料表，未來可以複製貼上直接用。
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(128))
+  
+    email = db.Column(db.String(120), unique=True)
+
+    # 串其他公司的ID搞兩公司資料關聯用的
+    external_id = db.Column(db.String(128))  # 他公司傳來的使用者ID
+    
+    # 這個帳號創造的時間註記
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+  
