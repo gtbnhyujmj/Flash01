8f60d898c27facf4913d4f6cd1491f6b3d8ff8b4
gtbnhyujmj <gtbnhyujmj@gmail.com>
2025-07-25 12:31:07 +0800

    Update main001.py

--- Flask main/main001.py
+++ Flask main/main001.py
@@ -3,7 +3,7 @@ from flask import Flask
 # render_template 是幹嘛的？
 # 它會去找 templates/ 資料夾裡的 HTML 檔案
 # 然後把那個網頁渲染（render）出來給使用者看
-# 所以你必須放一個叫 home.html 的檔案在 templates 裡
+# 所以你必須放一個叫 "xxx".html 的檔案在 templates 裡 (注意有"s")
 from flask import render_template
 
 
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # @ 跟 def 是雙胞胎，用來告訴 Flask，當使用者連到網站的某個網址時，就執行後面的定義式
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html')   # <<< 這裡用"home.html"示範
 
 
 if __name__ == '__main__':
